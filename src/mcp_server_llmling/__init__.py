53e7c85b63d614a7500b4bb5409cb7587512b755
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-27 09:07:55 +0100

    chore: use upathtools instead of fsspec-httpx

--- src/mcp_server_llmling/__init__.py
+++ src/mcp_server_llmling/__init__.py
@@ -2,11 +2,11 @@
 
 __version__ = "0.5.7"
 
-from fsspec_httpx import register
 
-register()
+import upathtools
 
 from mcp_server_llmling.server import LLMLingServer
 
+upathtools.register_http_filesystems()
 
 __all__ = ["LLMLingServer"]
