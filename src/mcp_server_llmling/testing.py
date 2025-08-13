0832f61c63921b4a0e283b9ad9956bfc34174479
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-08-13 04:35:11 +0200

    chore: typing

--- src/mcp_server_llmling/testing.py
+++ src/mcp_server_llmling/testing.py
@@ -3,12 +3,12 @@
 from __future__ import annotations
 
 
-def test_func(main_arg: str, opt1: str = "default", opt2: bool = False) -> str:
+def test_func(main_arg: str, opt1: str = "default", opt2: bool = False) -> str:  # noqa: PT028
     """Function purely for testing."""
     return f"Main: {main_arg}, Opt1: {opt1}, Opt2: {opt2}"
 
 
-def test_func_multi(main_arg: str, opt1: str = "default", opt2: bool = False) -> str:
+def test_func_multi(main_arg: str, opt1: str = "default", opt2: bool = False) -> str:  # noqa: PT028
     """Function with multiple parameters."""
     return f"Main: {main_arg}, Opt1: {opt1}, Opt2: {opt2}"
 
