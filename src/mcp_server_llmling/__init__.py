d8b616d48ea29b64dcfef0cf4563bb3c17930a5c
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:20:42 +0200

    chore: bump template

--- src/mcp_server_llmling/__init__.py
+++ src/mcp_server_llmling/__init__.py
@@ -1,10 +1,17 @@
-"""MCP protocol server implementation for LLMling."""
+"""mcp-server-llmling: MCP (Model context protocol) server with LLMling backend"""
 
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("mcp-server-llmling")
+__title__ = "mcp-server-llmling"
+__description__ = "MCP (Model context protocol) server with LLMling backend"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2024 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/mcp-server-llmling"
 
 import upathtools
 
